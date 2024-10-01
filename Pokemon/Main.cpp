b23395a566c78388ef4f0213fa9d4bf010edd832
Sayan11071998 <sayannandi11071998@gmail.com>
2024-10-02 00:30:34 +0530

    Choosing Your Legendary Pokemon!

--- Pokemon/Main.cpp
+++ Pokemon/Main.cpp
@@ -2,11 +2,18 @@
 #include <string>
 using namespace std;
 
-int main()
-{
+// Define an enum for Pokemon choices
+enum PokemonChoice {
+    Charmander,
+    Bulbasaur,
+    Squirtle,
+    InvalidChoice
+};
+
+int main() {
     // Variables to store player name and chosen Pokemon
     string player_name;
-    string chosen_pokemon;
+    PokemonChoice chosen_pokemon = InvalidChoice; // Default to an invalid choice
 
     // Introduction by the Professor
     cout << "Professor Oak: Hello there! Welcome to the world of Pokemon!" << endl;
@@ -14,14 +21,14 @@ int main()
     cout << "Professor Oak: But enough about me. Let's talk about you!" << endl;
 
     // Taking player name as input
-    cout << "Professor Oak: First, tell me, what is your name?" << endl;
+    cout << "Professor Oak: First, tell me, what’s your name?" << endl;
     cin >> player_name;
 
     cout << "Professor Oak: Ah, " << player_name << "! What a fantastic name!" << endl;
-    cout << "Professor Oak: You must be eager to start your adventure. But first, you will need a Pokemon of your own!" << endl;
+    cout << "Professor Oak: You must be eager to start your adventure. But first, you’ll need a Pokemon of your own!" << endl;
 
     // Presenting Pokemon choices
-    cout << "Professor Oak: I have three Pokemon here with me. They are all quite feisty!" << endl;
+    cout << "Professor Oak: I have three Pokemon here with me. They’re all quite feisty!" << endl;
     cout << "Professor Oak: Choose wisely..." << endl;
     cout << "1. Charmander - The fire type. A real hothead!" << endl;
     cout << "2. Bulbasaur - The grass type. Calm and collected!" << endl;
@@ -31,34 +38,44 @@ int main()
     cout << "Professor Oak: So, which one will it be? Enter the number of your choice: ";
     cin >> choice;
 
-    // Store the chosen Pokemon based on user input
+    // Map the integer choice to the corresponding enum value
     switch (choice) {
     case 1:
-        chosen_pokemon = "Charmander";
+        chosen_pokemon = Charmander;
+        break;
+    case 2:
+        chosen_pokemon = Bulbasaur;
+        break;
+    case 3:
+        chosen_pokemon = Squirtle;
+        break;
+    default:
+        chosen_pokemon = InvalidChoice;
+        break;
+    }
+
+    // Respond based on the chosen Pokemon
+    switch (chosen_pokemon) {
+    case Charmander:
         cout << "Professor Oak: A fiery choice! Charmander is yours!" << endl;
         break;
-
-    case 2:
-        chosen_pokemon = "Bulbasaur";
+    case Bulbasaur:
         cout << "Professor Oak: A fine choice! Bulbasaur is always ready to grow on you!" << endl;
         break;
-
-    case 3:
-        chosen_pokemon = "Squirtle";
+    case Squirtle:
         cout << "Professor Oak: Splendid! Squirtle will keep you cool under pressure!" << endl;
         break;
-
     default:
-        cout << "Professor Oak: Hmm, that does not seem right. Let me choose for you..." << endl;
-        chosen_pokemon = "Pikachu";
-        cout << "Professor Oak: Just kidding! Let's go with Pikachu, the surprise guest!" << endl;
+        cout << "Professor Oak: Hmm, that doesn't seem right. Let me choose for you..." << endl;
+        chosen_pokemon = Charmander; // Default to Charmander if invalid choice
+        cout << "Professor Oak: Just kidding! Let's go with Charmander, the fiery dragon in the making!" << endl;
         break;
     }
 
     // Concluding the first chapter
-    cout << "Professor Oak: " << chosen_pokemon << " and you, " << player_name << ", are going to be the best of friends!" << endl;
+    cout << "Professor Oak: " << (chosen_pokemon == Charmander ? "Charmander" : chosen_pokemon == Bulbasaur ? "Bulbasaur" : "Squirtle")
+        << " and you, " << player_name << ", are going to be the best of friends!" << endl;
     cout << "Professor Oak: Your journey begins now! Get ready to explore the vast world of Pokemon!" << endl;
-
 
     return 0;
 }
