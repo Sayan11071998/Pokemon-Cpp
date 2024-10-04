04fa396810fee72903294866de166fa5dc30bbae
Sayan11071998 <sayannandi11071998@gmail.com>
2024-10-04 22:11:52 +0530

    Set the stage for combat

--- Pokemon/Pokemon.cpp
+++ Pokemon/Pokemon.cpp
@@ -3,32 +3,60 @@
 #include <iostream>
 using namespace std;
 
-// Default constructor
 Pokemon::Pokemon() {
     name = "Unknown";
     type = PokemonType::NORMAL;
     health = 50;
 }
 
-// Parameterized constructor
 Pokemon::Pokemon(string p_name, PokemonType p_type, int p_health) {
     name = p_name;
     type = p_type;
     health = p_health;
 }
 
-// Copy constructor
 Pokemon::Pokemon(const Pokemon& other) {
     name = other.name;
     type = other.type;
     health = other.health;
 }
 
-// Destructor
 Pokemon::~Pokemon() {
     // Destructor logic (if any) goes here
 }
 
-void Pokemon::attack() {
-    cout << name << " attacks with a powerful move!" << endl;
+void Pokemon::attack(Pokemon& target) {
+    int damage = 10;
+    cout << name << " attacks " << target.name << " for " << damage << " damage!" << endl;
+    target.takeDamage(damage);
+}
+
+void Pokemon::takeDamage(int damage) {
+    health -= damage;
+    if (health < 0) {
+        health = 0;
+    }
+}
+
+bool Pokemon::isFainted() const {
+    return health <= 0;
+}
+
+void Pokemon::battle(Pokemon& playerPokemon, Pokemon& wildPokemon) {
+    cout << "A wild " << wildPokemon.name << " appeared!" << endl;
+
+    while (!playerPokemon.isFainted() && !wildPokemon.isFainted()) {
+        playerPokemon.attack(wildPokemon);
+
+        if (!wildPokemon.isFainted()) {
+            wildPokemon.attack(playerPokemon);
+        }
+    }
+
+    if (playerPokemon.isFainted()) {
+        cout << playerPokemon.name << "has fainted! You lose the battle." << endl;
+    }
+    else {
+        cout << "You defeated the wild " << wildPokemon.name << "!" << endl;
+    }
 }
--- Pokemon/Pokemon.hpp
+++ Pokemon/Pokemon.hpp
@@ -7,18 +7,16 @@ public:
     string name;
     PokemonType type;
     int health;
+    int maxHealth;
 
-    // Default constructor
     Pokemon();
-
-    // Parameterized constructor
     Pokemon(string p_name, PokemonType p_type, int p_health);
-
-    // Copy constructor
     Pokemon(const Pokemon& other);
 
-    // Destructor
     ~Pokemon();
 
-    void attack();
+    void attack(Pokemon &target);
+    void takeDamage(int damage);
+    bool isFainted() const;
+    void battle(Pokemon& playerPokemon, Pokemon& wildPokemon);
 };
