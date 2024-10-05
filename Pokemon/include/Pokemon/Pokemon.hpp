36a622def769332ffd6d69a1ef510aca89a094d4
Sayan11071998 <sayannandi11071998@gmail.com>
2024-10-06 00:01:36 +0530

    Add Multiple and Special Moves

--- Pokemon/include/Pokemon/Pokemon.hpp
+++ Pokemon/include/Pokemon/Pokemon.hpp
@@ -1,25 +1,29 @@
 #pragma once
 #include <string>
+#include <vector>
+using namespace std;
 
 namespace N_Pokemon {
 
+    struct Move;
     enum class PokemonType;
 
     class Pokemon {
-    public:
-        std::string name;
-        PokemonType type;
         int health;
         int maxHealth;
         int attackPower;
+        vector<Move> moves; // Store the list of moves
 
         Pokemon();
-        Pokemon(std::string p_name, PokemonType p_type, int p_health, int p_attackPower);
-        Pokemon(Pokemon* other);
+        Pokemon(string p_name, PokemonType p_type, int p_health, int p_attackPower);
 
-        bool isFainted() const;
         void heal();
-        void attack(Pokemon* target) = 0;
+        virtual void attack(Pokemon* target) = 0;
         void takeDamage(int damage);
+
+    protected:
+        // Base implementation for selecting and using a move
+        void selectAndUseMove(Pokemon* target);
+
     };
 }
