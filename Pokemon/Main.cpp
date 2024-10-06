3b472456296aafc867815569d408807c3b0bd13c
Sayan11071998 <sayannandi11071998@gmail.com>
2024-10-06 08:59:15 +0530

    Implement the Interface

--- Pokemon/Main.cpp
+++ Pokemon/Main.cpp
@@ -4,24 +4,6 @@
 #include <iostream>
 #include <limits> // Include this header to use std::numeric_limits
 
-class IStatusEffect {
-public:
-    // Apply the effect (e.g., poison, burn)
-    virtual void applyEffect(Pokemon* target) = 0;
-
-    // Get the name of the effect
-    virtual std::string getEffectName() = 0;
-
-    // Apply the changes due to effect after the end of each turn
-    // Returns true if the Pokémon can move, else false
-    virtual bool turnEndEffect(Pokemon* target) = 0;
-
-    // Remove the effect
-    virtual void clearEffect(Pokemon* target) = 0;
-
-    virtual ~IStatusEffect() = default;
-}
-
 using namespace N_Character;
 using namespace N_Player;
 
